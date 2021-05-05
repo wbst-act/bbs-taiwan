8125a806d69157b8a9abc91bc8f3f9ba96e88f43
Kenny Chou <kenny@kramxel.com>
2021-05-05 16:15:11 +0800

    deploy

---
+++ precache-manifest.8631d5129daadc4e95c345de87f41a2c.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45e9a64f5a488efee66",
+    "url": "/bbs-taiwan/css/chunk-029cbe98.0f5f7bce.css"
+  },
+  {
+    "revision": "f6eb2e854e926fcefdf7",
+    "url": "/bbs-taiwan/css/chunk-099a9edf.062cd1b7.css"
+  },
+  {
+    "revision": "95e258bd8cbdbba97f8e",
+    "url": "/bbs-taiwan/css/chunk-19505115.11e5c157.css"
+  },
+  {
+    "revision": "8451617f2e3b8607e910",
+    "url": "/bbs-taiwan/css/chunk-31d70396.9deb4d24.css"
+  },
+  {
+    "revision": "cf75694e870e33d238bc",
+    "url": "/bbs-taiwan/css/chunk-363e6b50.108e3324.css"
+  },
+  {
+    "revision": "04ae08dcf8eacc4f0904",
+    "url": "/bbs-taiwan/css/chunk-3aee5d93.f5756e02.css"
+  },
+  {
+    "revision": "252430b59c03adfa1621",
+    "url": "/bbs-taiwan/css/chunk-3c5aa9c6.f32eaac2.css"
+  },
+  {
+    "revision": "6483bca493cf667f8f1e",
+    "url": "/bbs-taiwan/css/chunk-41ad831f.a38e5248.css"
+  },
+  {
+    "revision": "88d398ba1d20753ea48c",
+    "url": "/bbs-taiwan/css/chunk-5ac30e18.0382f3fd.css"
+  },
+  {
+    "revision": "3486c035b666fb70ab13",
+    "url": "/bbs-taiwan/css/chunk-60729070.1434bd55.css"
+  },
+  {
+    "revision": "d3ca8b14300cc000d726",
+    "url": "/bbs-taiwan/css/chunk-62eb3b55.1a8e324a.css"
+  },
+  {
+    "revision": "83352b0fc66ccb82926a",
+    "url": "/bbs-taiwan/css/chunk-aac3b08a.af1b91ff.css"
+  },
+  {
+    "revision": "6ed3f8874d3a3c3e3981",
+    "url": "/bbs-taiwan/css/chunk-b3d041be.e4283073.css"
+  },
+  {
+    "revision": "12ad3ede25e33feafe85",
+    "url": "/bbs-taiwan/css/chunk-e99f194e.513229a5.css"
+  },
+  {
+    "revision": "a88a1f78b72e1f1a11b4",
+    "url": "/bbs-taiwan/css/chunk-ecbf78c4.27d7111a.css"
+  },
+  {
+    "revision": "d1794558ba5c01e5eb0b",
+    "url": "/bbs-taiwan/css/chunk-vendors.e669275c.css"
+  },
+  {
+    "revision": "67a51573d21219bd48cd5c7dfcea76f1",
+    "url": "/bbs-taiwan/data/bbs_bird_2021.json"
+  },
+  {
+    "revision": "8a3a1dd88cf4fdef69a879f77f932d77",
+    "url": "/bbs-taiwan/data/bbs_plot.json"
+  },
+  {
+    "revision": "ab6e1ad445f63e913a1bf8fc2e513bf7",
+    "url": "/bbs-taiwan/data/ebird_bird.json"
+  },
+  {
+    "revision": "809e3c8d3d041cc2e846566d13b0ddf7",
+    "url": "/bbs-taiwan/data/ebird_family.json"
+  },
+  {
+    "revision": "ac32d53cd2ae00838944b680a09d3488",
+    "url": "/bbs-taiwan/data/plot_bird.json"
+  },
+  {
+    "revision": "f10efb08187562a1e8ca4619b7520e6f",
+    "url": "/bbs-taiwan/index.html"
+  },
+  {
+    "revision": "2582463606a321d60467",
+    "url": "/bbs-taiwan/js/app.65809d33.js"
+  },
+  {
+    "revision": "d45e9a64f5a488efee66",
+    "url": "/bbs-taiwan/js/chunk-029cbe98.d0940153.js"
+  },
+  {
+    "revision": "f6eb2e854e926fcefdf7",
+    "url": "/bbs-taiwan/js/chunk-099a9edf.4f0aba1d.js"
+  },
+  {
+    "revision": "95e258bd8cbdbba97f8e",
+    "url": "/bbs-taiwan/js/chunk-19505115.1178b177.js"
+  },
+  {
+    "revision": "8451617f2e3b8607e910",
+    "url": "/bbs-taiwan/js/chunk-31d70396.6e1cff9b.js"
+  },
+  {
+    "revision": "cf75694e870e33d238bc",
+    "url": "/bbs-taiwan/js/chunk-363e6b50.396424a0.js"
+  },
+  {
+    "revision": "04ae08dcf8eacc4f0904",
+    "url": "/bbs-taiwan/js/chunk-3aee5d93.d293243c.js"
+  },
+  {
+    "revision": "252430b59c03adfa1621",
+    "url": "/bbs-taiwan/js/chunk-3c5aa9c6.93d9c161.js"
+  },
+  {
+    "revision": "6483bca493cf667f8f1e",
+    "url": "/bbs-taiwan/js/chunk-41ad831f.de9b1684.js"
+  },
+  {
+    "revision": "996ab6fa00a9179ba70f",
+    "url": "/bbs-taiwan/js/chunk-4a284098.e47615c1.js"
+  },
+  {
+    "revision": "b4cb3de0c3b2058d1a59",
+    "url": "/bbs-taiwan/js/chunk-5a03cd10.24fd2a70.js"
+  },
+  {
+    "revision": "88d398ba1d20753ea48c",
+    "url": "/bbs-taiwan/js/chunk-5ac30e18.cb7d9e1d.js"
+  },
+  {
+    "revision": "3486c035b666fb70ab13",
+    "url": "/bbs-taiwan/js/chunk-60729070.73d4e04e.js"
+  },
+  {
+    "revision": "d3ca8b14300cc000d726",
+    "url": "/bbs-taiwan/js/chunk-62eb3b55.c6224f1f.js"
+  },
+  {
+    "revision": "83352b0fc66ccb82926a",
+    "url": "/bbs-taiwan/js/chunk-aac3b08a.d292f1d8.js"
+  },
+  {
+    "revision": "6ed3f8874d3a3c3e3981",
+    "url": "/bbs-taiwan/js/chunk-b3d041be.91c12f36.js"
+  },
+  {
+    "revision": "12ad3ede25e33feafe85",
+    "url": "/bbs-taiwan/js/chunk-e99f194e.2be206f8.js"
+  },
+  {
+    "revision": "a88a1f78b72e1f1a11b4",
+    "url": "/bbs-taiwan/js/chunk-ecbf78c4.94f912e1.js"
+  },
+  {
+    "revision": "d1794558ba5c01e5eb0b",
+    "url": "/bbs-taiwan/js/chunk-vendors.18c88055.js"
+  },
+  {
+    "revision": "f77377c4d05289fc7e88a20974d534cb",
+    "url": "/bbs-taiwan/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bbs-taiwan/robots.txt"
+  }
+]);
